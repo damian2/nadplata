016fc0e2075992f62bfd86426f45d19029cc5dc2
Damian Matusiak <damian12124@gmail.com>
2023-04-08 20:02:04 +0200

    table improvement

--- src/InstallmentTable.js
+++ src/InstallmentTable.js
@@ -1,7 +1,14 @@
-import React from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import React, { useState } from "react";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from "@mui/material";
 
 const InstallmentTable = ({ installments }) => {
+    const [displayAll, setDisplayAll] = useState(false);
+    const showAll = installments.length > 20 && !displayAll;
+
+    const handleClick = () => {
+        setDisplayAll(true);
+    };
+
     return (
         <TableContainer component={Paper}>
             <Table>
@@ -16,16 +23,32 @@ const InstallmentTable = ({ installments }) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {installments.map((installment, index) => (
-                        <TableRow key={index}>
-                            <TableCell>{index + 1}</TableCell>
-                            <TableCell>{installment.payment.toFixed(2)}</TableCell>
-                            <TableCell>{(installment.payment + installment.extraPayment).toFixed(2)}</TableCell>
-                            <TableCell>{installment.remaining.toFixed(2)}</TableCell>
-                            <TableCell>{installment.principal.toFixed(2)}</TableCell>
-                            <TableCell>{installment.interest.toFixed(2)}</TableCell>
-                        </TableRow>
-                    ))}
+                    {installments.map((installment, index) => {
+                        if (showAll && (index >= 5 && index < installments.length - 5)) {
+                            if (index === 5) {
+                                return (
+                                    <TableRow key="show-all">
+                                        <TableCell colSpan={6} align="center">
+                                            <Button variant="contained" onClick={handleClick}>
+                                                Pokaż wszystko
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            }
+                            return null;
+                        }
+                        return (
+                            <TableRow key={index}>
+                                <TableCell>{index + 1}</TableCell>
+                                <TableCell>{installment.payment.toFixed(2)}</TableCell>
+                                <TableCell>{(installment.payment + installment.extraPayment).toFixed(2)}</TableCell>
+                                <TableCell>{installment.remaining.toFixed(2)}</TableCell>
+                                <TableCell>{installment.principal.toFixed(2)}</TableCell>
+                                <TableCell>{installment.interest.toFixed(2)}</TableCell>
+                            </TableRow>
+                        );
+                    })}
                 </TableBody>
             </Table>
         </TableContainer>
